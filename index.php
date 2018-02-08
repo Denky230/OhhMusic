69f0491da164c6b8c26ac1e1e71668db3c7efdbc
Denky <>
2018-02-08 15:24:25 +0100

    Oscar commit test (cambiando autor)

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "this is a test yo!";
+        echo "this is a test yo! x2";
         ?>
     </body>
 </html>
