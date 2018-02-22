f3b6e2535403970bfd5ead1461531a806d1266b8
Denky <>
2018-02-22 17:00:12 +0100

    Intentando adaptar las subpags a los iframes

--- js/slides.js
+++ js/slides.js
@@ -1,6 +1,7 @@
-
 var slide_index = 1;
+
 displaySlides(slide_index);
+
 function nextSlide(n){
     displaySlides(slide_index += n);
 }
@@ -8,11 +9,14 @@ function currentSlide(n){
     displaySlides(slide_index = n);
 }
 function displaySlides(n){
-    var i;
     var slides = document.getElementsByClassName("showSlide");
-    if(n > slides.length){slide_index = 1}
-    if(n < 1){slide_index = slides.length}
-    for(i=0; i < slides.length; i++){
+    if (n > slides.length){
+        slide_index = 1;
+    }
+    if (n < 1){
+        slide_index = slides.length;
+    }
+    for (var i = 0; i < slides.length; i++){
         slides[i].style.display = "none";
     }
     slides([slide_index - 1]).setAttribute("style", "display:block");
