335dde807ea2ed126bc243bb31c1b6028e0fcada
Daniel Tarrasso <dani_tarrassotoscano@hotmail.com>
2018-02-23 11:24:42 +0100

    slides js

--- js/slides.js
+++ js/slides.js
@@ -1,19 +1,36 @@
 
-var slide_index = 1;
-displaySlides(slide_index);
-function nextSlide(n){
-    displaySlides(slide_index += n);
+var slider=$('#slider');
+var siguiente = $('#btn-next');
+var anterior = $('#btn-prev');
+
+$('#slider section:last').insertBefore('#slider section:first');
+slider.css('margin-left', '-'+100+'%');
+
+function moverD(){
+    slider.animate({marginleft: '-'+200+'%'},700,function(){
+        $('#slider section:first').insertAfter('#slider section:last');
+        slider.css('margin-left', '-'+100+'%');
+    });
 }
-function currentSlide(n){
-    displaySlides(slide_index = n);
+
+function moverI(){
+    slider.animate({marginleft:0},700,function(){
+        $('#slider section:last').insertBefore('#slider section:first');
+        slider.css('margin-left', '-'+100+'%');
+    });
 }
-function displaySlides(n){
-    var i;
-    var slides = document.getElementsByClassName("showSlide");
-    if(n > slides.length){slide_index = 1}
-    if(n < 1){slide_index = slides.length}
-    for(i=0; i < slides.length; i++){
-        slides[i].style.display = "none";
-    }
-    slides([slide_index - 1]).setAttribute("style", "display:block");
+
+siguiente.on('click', function(){
+    moverD();
+});
+
+anterior.on('click',function(){
+    moverI();
+});
+
+function autoplay(){
+    interval = setInterval(function(){
+        moverD();
+    }, 5000);
 }
+autoplay();
