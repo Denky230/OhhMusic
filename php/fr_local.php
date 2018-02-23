a48f3d994c5c5fd9b1484a944f79a1eca9deb588
Daniel Tarrasso <dani_tarrassotoscano@hotmail.com>
2018-02-23 16:56:07 +0100

    Local site create concert

--- php/fr_local.php
+++ php/fr_local.php
@@ -7,16 +7,27 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="../css/base.css"/>
-        <title></title>
+        <link rel="stylesheet" href="../css/local.css"/>
+        <title>Local</title>
     </head>
     <body>
-        <header>
-            <div id="langs">ESPAÑOL</div>
-            <h1><center>OHH MUSIC</center></h1>
-        </header>
-        <aside id="frame_left">
-            <div id="adBanner_left">ad here</div>
-        </aside>
+        <aside id="frame_right">
+            <div id="concert_creation">
+                <h2>Creacion concierto:</h2><br><br>
+                <div id="create_concert">
+                    <form method="post">
+                        <h4>Fecha del concierto:</h4>
+                        <input type="date" name="concert_date"><br>
+                        <h4>Hora:</h4>
+                        <input type="time" name="concert_time"><br>
+                        <h4>Genero:</h4>
+                        <input type="text" name="genre"><br>
+                        <h4>Tarifa:</h4>
+                        <input type="number" name="price"><br><br>
+                        <input type="submit" name="button" value="Enviar">
+                    </form>
+                </div>                
+            </div>
+        </aside>        
     </body>
 </html>
