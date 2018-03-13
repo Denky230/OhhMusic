70fd918ae48b3b87ea3e96713ae9b978a3368858
Daniel Tarrasso <dani_tarrassotoscano@hotmail.com>
2018-03-13 20:05:59 +0100

    List-Grid Switch (needs some modifications tho..)

--- js/local.js
+++ js/local.js
@@ -1,7 +1,22 @@
-$('button').click(function(){
-    if($(this).hasClass('list')){
-        $('#main #concert_box').removeClass('grid').addClass('list');
-    }else if($(this).hasClass('grid')){
-        $('#main #concert_box').removeClass('list').addClass('grid');
+//local.js
+
+$('#header img').on('click',function(){
+    if ( $(this).hasClass('view-list') ) {
+        $('#frame').removeClass('grid').addClass('list');
+    } else if ( $(this).hasClass('view-grid') ) {
+        $('#frame').removeClass('list').addClass('grid');
     }
+});
+
+$('.frame').each(function(){
+    var images = ['random.jpg','https://goo.gl/b93Zu','https://goo.gl/y4kdu',
+        'https://goo.gl/eh4OX','https://goo.gl/aR2Y0','https://goo.gl/L16VF'];
+    $(this).find('.image')
+        .css({ 'background-image': 'url('+images[Math.floor(Math.random()*images.length)]+')' });
+    var like = 100,
+        likes = Math.floor(Math.random() * like) + 1;
+    $(this).find('.likes').text(likes);
+    var comment = 50,
+        comments = Math.floor(Math.random() * comment) + 1;
+    $(this).find('.comments').text(comments);
 });
