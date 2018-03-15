4d81375e2be598e0a0bff3a476e1c3ba19b1f989
Andrea <Andrea@localhost>
2018-03-15 15:58:18 +0100

    Creada las listas de concierto y músico (finished)

--- php/fr_fan.php
+++ php/fr_fan.php
@@ -10,7 +10,7 @@ require_once 'dmlFunctions.php';
 <html>
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="../css/fan1.css"/>
+        <link rel="stylesheet" href="../css/fan.css"/>
         <title>Fan</title>
     </head>
     <body style="background-color: white">
@@ -18,89 +18,43 @@ require_once 'dmlFunctions.php';
            <div id="concerts">
                <div id="list_concerts">
                     <h2>Listado de conciertos</h2> 
-                    <?php
-               
-                    $concerts = selectAllConcerts();
-               
-                    echo "<table>";
-
-                    echo "<tr>";
-                    echo "<th>Id Concert</th><th>State</th><th>Concert Date</th><th>Concert Time</th>"
-                    . "<th>Genre</th><th>Payment</th><th>Id Local</th><th>Id Musician</th>";
-                    echo "</tr>";
-
-                    while ($fila = mysqli_fetch_assoc($concerts)) {
-                    echo "<tr>";
-                    foreach ($fila as $valor) {
-                    echo "<td>$valor</td>";
-                    }
-                    echo "</tr>";
-                    }
-                    echo "</table>";
-                    ?>
+                    <div id="concert_box">
+                        <img id="concert_img" src="" alt="">
+                        <div id="concert_info">
+                        </div>
+                    </div>
+                    <div id="concert_box">
+                        <img id="concert_img" src="" alt="">
+                        <div id="concert_info">
+                        </div>
+                    </div>
+                    <div id="concert_box">
+                        <img id="concert_img" src="" alt="">
+                        <div id="concert_info">
+                        </div>
+                    </div>
                </div>
                <div id="list_musicians">
-                    <h2>Listado de músicos</h2>
+                    
+                   
+                   <h2>Listado de músicos</h2>
+                    <div id="concert_box">
+                        <img id="concert_img" src="" alt="">
+                        <div id="concert_info">
+                        </div>
+                    </div>
+                    <div id="concert_box">
+                        <img id="concert_img" src="" alt="">
+                        <div id="concert_info">
+                        </div>
+                    </div>
+                    <div id="concert_box">
+                        <img id="concert_img" src="" alt="">
+                        <div id="concert_info">
+                        </div>
+                    </div>
                </div>
            </div>
-            
-               
-               <!--     <div id="concert_box">
-                    <img id="concert_img" src="../media/0002045178_10.jpg" alt="">
-                    <div id="concert_info">
-                        <h2>NombreLocal</h2>
-                        <h2>LugarLocal</h2>
-                        <h2>TelfLocal</h2>
-                    </div>
-                </div>
-                <div id="concert_box">
-                    <img id="concert_img" src="" alt="">
-                    <div id="concert_info">
-                    </div>
-                </div>
-                <div id="concert_box">
-                    <img id="concert_img" src="" alt="">
-                    <div id="concert_info">
-
-                    </div>
-                </div>
-                <div id="concert_box">
-                    <img id="concert_img" src="" alt="">
-                    <div id="concert_info">
-
-                    </div>
-                </div>
-                <div id="concert_box">
-                    <img id="concert_img" src="" alt="">
-                    <div id="concert_info">
-
-                    </div>
-                </div>
-                <div id="concert_box">
-                    <img id="concert_img" src="" alt="">
-                    <div id="concert_info">
-
-                    </div>
-                </div>
-                <div id="concert_box">
-                    <img id="concert_img" src="" alt="">
-                    <div id="concert_info">
-
-                    </div>
-                </div>
-                <div id="concert_box">
-                    <img id="concert_img" src="" alt="">
-                    <div id="concert_info">
-
-                    </div>
-                </div>
-                <div id="concert_box">
-                    <img id="concert_img" src="" alt="">
-                    <div id="concert_info">
-
-                    </div>
-                </div>-->
-            
         </div>
     </body>
 </html>
