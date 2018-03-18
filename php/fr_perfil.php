b875459828d0a22c9752a7a93a9e30888cd3044b
Daniel Tarrasso <dani_tarrassotoscano@hotmail.com>
2018-03-18 14:56:29 +0100

    slider ready to go. need test of profile info with bbdd

--- php/fr_perfil.php
+++ php/fr_perfil.php
@@ -4,40 +4,7 @@ session_start();
 ?>
 <!DOCTYPE html>
 <?php
-    if (isset($_SESSION["type"])) {
-        switch ($_SESSION["type"]) {
-            case 1:
-                echo("<div id='profile_specific_info'>");
-                        $musicians = selectFields("artist_name, genre, surname, phone, web, group_size", "musician");
-                        $musician = mysqli_fetch_assoc($musicians);
-                echo("Artist Name: <div id='artist_name'>" . $musician['artist_name'] ."</div>
-                        Genre: <div id='genre'>". $musician['genre'] ."</div>
-                        Surname: <div id='surname'>" . $musician['surname'] . "</div>
-                        Phone: <div id='phone'>" . $musician['phone'] ."</div>
-                        Webpage: <div id='webpage'>" . $musician['web'] . "</div>
-                        Group Size: <div id='group_size'>" . $musician['group_size'] . "</div>
-                        </div>");
-                break;
-            case 2:
-                echo("<div id='profile_specific_info'>");
-                        $locals = selectFields("phone, capacity, web", "local");
-                        $local = mysqli_fetch_assoc($local);
-                echo("Phone number: <div id='phone'>" . $local['phone'] . "</div>
-                        Max Capacity: <div id='capacity'>" . $local['capacity'] . "</div>
-                        Webpage: <div id='webpage'>" . $local['web'] . "</div>
-                        </div>");
-                break;
-            case 3:
-                echo("<div id='profile_specific_info'>");
-                        $fans = selectFields("phone, address, surname", "fan");
-                        $fan = mysqli_fetch_assoc($fans);
-                echo("Phone number: <div id='phone'>" . $fan['phone'] . "</div>
-                        Address: <div id='address'>" . $fan['address'] . "</div>
-                        Surname: <div id='surname'>" . $fan['surname'] . "</div>
-                        </div>");
-                break;
-        }
-    }
+if (isset($_SESSION["type"])) {
 ?>
 <html>
 <head>
@@ -49,18 +16,48 @@ session_start();
     <div id="main">
         <div id="display">
             <div id="container">
-                <div id="profile_general_info">
-                    <div id="image_box"></div>
-                    Username: <div id="username">Danielo</div>
-                    Name: <div id="name">Dani</div>
-                    E-mail: <div id="email">danielo_@hotmali.com</div>
-                    City: <div id="city">BCN</div>
+                <div id="profile_general_info"><?php $users = selectFields("image, username, name, email, city", "user");
+                                                     $user = mysqli_fetch_assoc($users);?>
+                    <div id="image_box"><?php $user['image']; ?></div>
+                    Username: <div id="username"><?php $user['username']; ?></div>
+                    Name: <div id="name"><?php $user['name']; ?></div>
+                    E-mail: <div id="email"><?php $user['email']; ?></div>
+                    City: <div id="city"><?php $user['city']; ?></div>
                 </div>
-                <div id="profile_specific_info">
-
+                <div id="profile_specific_info"><?php
+                        switch ($_SESSION["type"]) {
+                            case 1:
+                                $musicians = selectFields("artist_name, genre, surname, phone, web, group_size", "musician");
+                                $musician = mysqli_fetch_assoc($musicians);
+                                echo("Artist Name: <div id='artist_name'>" . $musician['artist_name'] ."</div>
+                        Genre: <div id='genre'>". $musician['genre'] ."</div>
+                        Surname: <div id='surname'>" . $musician['surname'] . "</div>
+                        Phone: <div id='phone'>" . $musician['phone'] ."</div>
+                        Webpage: <div id='webpage'>" . $musician['web'] . "</div>
+                        Group Size: <div id='group_size'>" . $musician['group_size'] . "</div>");
+                                break;
+                            case 2:
+                                $locals = selectFields("phone, capacity, web", "local");
+                                $local = mysqli_fetch_assoc($locals);
+                                echo("Phone number: <div id='phone'>" . $local['phone'] . "</div>
+                        Max Capacity: <div id='capacity'>" . $local['capacity'] . "</div>
+                        Webpage: <div id='webpage'>" . $local['web'] . "</div>");
+                                break;
+                            case 3:
+                                $fans = selectFields("phone, address, surname", "fan");
+                                $fan = mysqli_fetch_assoc($fans);
+                                echo("Phone number: <div id='phone'>" . $fan['phone'] . "</div>
+                        Address: <div id='address'>" . $fan['address'] . "</div>
+                        Surname: <div id='surname'>" . $fan['surname'] . "</div>");
+                                break;
+                        }
+                    ?>
                 </div>
             </div>
         </div>
     </div>
 </body>
 </html>
+<?php
+}
+?>
--- php/slider.php
+++ php/slider.php
@@ -5,22 +5,23 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        <link type="text/css" href="../css/slider.css">
-    </head>
-    <body>
-        <div id="c-slider">
-            <div id="slider">
-                <section><img src="../images/maxresdefault.jpg" alt=""/></section>
-                <section><img src="../images/" alt=""/></section>
-                <section><img src="../images/pexels-photo.jpeg" alt=""/></section>
-                <section><img src="../images/sO0mRdKW.jpeg" alt=""/></section>
-            </div>
-            <div id="btn-prev">&#60</div>
-            <div id="btn-next">&#62</div>
-        </div>
-        <script src="../js/slides.js"></script>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link href="../css/slider.css" type="text/css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+</head>
+<body>
+<div id="c-slider">
+    <div id="slider">
+        <section><img src="../images/DzrMPF.jpg" alt=""/></section>
+        <section><img src="../images/music-poster.jpg" alt=""/></section>
+        <section><img src="../images/SEVRAGE-TOUR.jpg" alt=""/></section>
+        <section><img src="../images/Vintage.jpg" alt=""/></section>
+    </div>
+    <div id="btn-prev">&#60</div>
+    <div id="btn-next">&#62</div>
+</div>
+<script src="../js/slides.js"></script>
+</body>
 </html>
