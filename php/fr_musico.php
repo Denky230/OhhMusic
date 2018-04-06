8dac71f7768038b3393c118b9f1e793127f31630
Andrea <Andrea@localhost>
2018-04-06 17:01:37 +0200

    ordenar conciertos

--- php/fr_musico.php
+++ php/fr_musico.php
@@ -8,11 +8,20 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../css/frame.css"/>
-        <link rel="stylesheet" href="../css/musico.css"/>
+        <link rel="stylesheet" href="../css/Musico.css"/>
     </head>
     <body>
         <div id="main">
             <div id="frameTitle"><h2>CONCIERTOS</h2></div>
+            <div id="sort_bg">
+            <div id="sort_select">
+                <select name="userType_select" id="userType_select">
+                    <option value="1">Más reciente</option>
+                    <option value="2">Más antiguo</option>
+                </select>
+                <button type="button" id="sort_select_btn">Ordenar</button>
+            </div>
+        </div>
             <div id="concerts">
                 <div id="concert_box">
                     <img id="concert_img" src="../media/random.jpg" alt="">
