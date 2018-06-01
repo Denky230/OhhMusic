fd84e2b238c554a667244ed541a7060ca246a054
Denky <User@DESKTOP-4652H0E>
2018-06-01 20:16:41 +0200

    F - order by fields - WiP

--- js/fan.js
+++ js/fan.js
@@ -2,19 +2,25 @@ $(document).ready(function() {
 	drawContent(document.getElementById("concert"));
 });
 
-function drawContent(title) {
+function drawContent(title, orderByField, order) {
+    // Set default values
+    orderByField = orderByField == undefined ? 1 : ++orderByField;
+    if (order == undefined) order = "ASC";
+
 	// Underline active title
     $("#frameTitle div").css("text-decoration", "none");
     title.style.textDecoration = "underline";
 
-    ajax("ajax_fan.php?draw=" + title.getAttribute("id")).onreadystatechange = function() {
+    ajax("ajax_fan.php?draw=" + title.getAttribute("id") + "&orderByField=" + orderByField + "&order=" + order).onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("concerts").innerHTML = this.responseText;
+            $(".header").click(orderContent);
         }
     };
 }
 
 function voteMusician(musician, value) {
+    console.log("musician");
     ajax("ajax_fan.php?voteMusician=" + musician +"&value="+ value).onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             drawContent(document.getElementById("musician"));
@@ -23,9 +29,17 @@ function voteMusician(musician, value) {
 }
 
 function voteConcert(concert, value) {
+    console.log("concert");
     ajax("ajax_fan.php?voteConcert=" + concert +"&value="+ value).onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {            
             drawContent(document.getElementById("concert"));
         }
     };
+}
+
+function orderContent() {
+    var order = $(this).children("img").attr("id");
+    if (order == "DESC")
+        drawContent(document.getElementById("musician"), $(this).attr("id"), "ASC");
+    else drawContent(document.getElementById("musician"), $(this).attr("id"), "DESC");
 }
