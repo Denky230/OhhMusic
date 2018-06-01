d73d0fb70e82876b522a00b474e03642d3f586d7
Denky <User@DESKTOP-4652H0E>
2018-06-02 00:19:51 +0200

    Irrelevant

--- js/home.js
+++ js/home.js
@@ -1,37 +1,29 @@
+
 $(document).ready(function(){
-	$("input[type=submit]").hover(highlight, lowlight);
-	$("#changeimage").click(changeImage());
+    $("input[type=submit]").hover(highlight, lowlight);
 });
 
 var xDiff = 30;
 var hDiff = 10;
 var highlighted = false;
 function highlight(){
-	if (!$(this).is(":animated")){
-		$(this).animate({
-			top: hDiff/2,
-			left: xDiff,
-			height: hDiff
-			}, "fast");
-		$(this).css("z-index", "1");
-		highlighted = true;
-	}
+    if (!$(this).is(":animated")){
+        $(this).animate({
+                top: "-=" + hDiff/2 + "px",
+                left: "-=" + xDiff + "px",
+                height: "+=" + hDiff + "px"
+                }, "fast");
+        $(this).css("z-index", "1");
+        highlighted = true;
+    }
 }
 function lowlight(){
-	if (highlighted)
-		$(this).animate({
-		top: "+=" + hDiff/2 + "px",
-		left: "+=" + xDiff + "px",
-		height: "-=" + hDiff + "px"
-		}, "fast");
-	$(this).css("z-index", "0");
-	highlighted = false;
-}
-function changeImage(){
-	var image = document.getElementById('changeimage');
-    if(image.src.match("icons8-corazones-40.png")){
-        image.src="icons8-corazones-40(1).png";
-    }else{
-        image.src="icons8-corazones-40.png";
-    }
+    if (highlighted)
+        $(this).animate({
+                top: "+=" + hDiff/2 + "px",
+                left: "+=" + xDiff + "px",
+                height: "-=" + hDiff + "px"
+                }, "fast");
+    $(this).css("z-index", "0");
+    highlighted = false;
 }
